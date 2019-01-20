db81437166ce657e6a045d7e5aecf011cfa4c9cd
paulowebphp <paulowebphp@gmail.com>
2019-01-20 19:27:01 -0200

    Percorrendo os inputs do formulário com forEach

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert("Hi ");
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+var user = {};
+
+fields.forEach( function(field, index)
+{
+    if( field.name == "gender" )
+    {
+        if( field.checked )
+        {
+            user[field.name] = field.value;
+
+        }//end if
+    
+    }//end if
+    else
+    {
+        user[field.name] = field.value;
+    }//end else
+
+});//end forEach
+
+console.log(user);
