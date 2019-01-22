4d0b63e27060d3b04c468c50c9dbee112aed215b
paulowebphp <paulowebphp@gmail.com>
2019-01-22 12:41:08 -0200

    Configurando o DELETAR usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -280,7 +280,7 @@ class UserController
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
     
         `;//end innerHTML
@@ -309,6 +309,20 @@ class UserController
 
     addEventsTr( tr )
     {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e =>
+        {
+
+            if( confirm("Deseja excluir este usuário?") )
+            {
+
+                tr.remove();
+
+                this.updateCount();
+
+            }//end if
+
+        });//end querySelector
 
         tr.querySelector(".btn-edit").addEventListener("click", e =>
         {
